3cc4a1c0559ae9a684fdb76df64f6102e5d4f861
CPA <CPA@qq.com>
2025-04-19 12:59:07 +0800

    feat: 在test.cpp中添加Solution类和func函数
    添加了Solution类及其私有成员变量a和b，并实现了func函数以访问和输出这些变量。同时，在main函数中调用了func函数以测试其功能。

--- test.cpp
+++ test.cpp
@@ -1,7 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+friend void func();
+private:
+    int a,b;
+};
+void func(){
+    Solution s;
+    s.a=1;
+    cout<<s.a<<endl; //hell
+    s.b=2; 
+    cout<<s.b<<endl;
+}
 int main() {
     cout << "Hello World!" << endl;
-    cout<<"ä½ å¥½ä¸–ç•Œ"<<endl;
+    cout<<"ÄãºÃÊÀ½ç"<<endl;
     cout<<"Hello World!"<<endl; //hell
+    func();
 }
